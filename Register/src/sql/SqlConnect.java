64da12ea878127ea792b014080a7baf563e167b4
RadGamma <andrew.frazee@gmail.com>
2016-09-23 12:48:36 -0400

    and this guy too

---
+++ Register/src/sql/SqlConnect.java
@@ -0,0 +1,134 @@
+package sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+public class SqlConnect {
+  private Connection connect = null;
+  private Statement statement = null;
+  private PreparedStatement preparedStatement = null;
+  private ResultSet resultSet = null;
+
+  public void readDataBase() throws Exception {
+    try {
+      // This will load the MySQL driver, each DB has its own driver
+      Class.forName("com.mysql.jdbc.Driver");
+      // Setup the connection with the DB
+      connect = DriverManager
+          .getConnection("jdbc:mysql://192.168.0.11:3306/test");
+             // + "user=sqluser&password=sqluserpw");
+
+      // Statements allow to issue SQL queries to the database
+      statement = connect.createStatement();
+      // Result set get the result of the SQL query
+      resultSet = statement
+          .executeQuery("select * from JRegister");
+     // writeResultSet(resultSet);
+
+      // PreparedStatements can use variables and are more efficient
+      preparedStatement = connect
+          .prepareStatement("insert into JRegister (Register_Date,Register_Detail,Register_Pos_Value,"
+          		+ "Register_Neg_Value,Register_Balance,Purchase_Code) values (?,?,?,?,?,?);");
+      // Parameters start with 1
+      preparedStatement.setString(1, "99/99/9999");
+      preparedStatement.setString(2, "Cheese");
+      preparedStatement.setDouble(3, 00.00);
+      preparedStatement.setDouble(4, 00.00);
+      preparedStatement.setDouble(5, 00.00);
+      preparedStatement.setString(6, "002");
+      preparedStatement.executeUpdate();
+
+      //preparedStatement = connect
+      //    .prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from feedback.comments");
+      //resultSet = preparedStatement.executeQuery();
+      //writeResultSet(resultSet);
+
+      // Remove again the insert comment
+     // preparedStatement = connect
+     // .prepareStatement("delete from feedback.comments where myuser= ? ; ");
+     // preparedStatement.setString(1, "Test");
+     // preparedStatement.executeUpdate();
+      
+     // resultSet = statement
+     // .executeQuery("select * from feedback.comments");
+      writeRow(resultSet);
+      writeMetaData(resultSet);
+      
+    } catch (Exception e) {
+      throw e;
+    } finally {
+      close();
+    }
+
+  }
+
+  private void writeRow(ResultSet resultSet) throws SQLException {
+	  //   Now get some metadata from the database
+	  // Result set get the result of the SQL query
+	    
+	  System.out.println("The columns in the table are: ");
+	    
+	  System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
+	  while  (resultSet.next()){
+	    System.out.println("Row  "+ resultSet.getString("Register_Balance"));
+	    System.out.println("Row  "+ resultSet.getString("Register_Date"));
+	  }
+	}
+  
+  private void writeMetaData(ResultSet resultSet) throws SQLException {
+    //   Now get some metadata from the database
+    // Result set get the result of the SQL query
+    
+    System.out.println("The columns in the table are: ");
+    
+    System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
+    for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
+      System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
+    }
+  }
+
+  private void writeResultSet(ResultSet resultSet) throws SQLException {
+    // ResultSet is initially before the first data set
+    while (resultSet.next()) {
+      // It is possible to get the columns via name
+      // also possible to get the columns via the column number
+      // which starts at 1
+      // e.g. resultSet.getSTring(2);
+     // String user = resultSet.getString("myuser");
+     // String website = resultSet.getString("webpage");
+    //  String summary = resultSet.getString("summary");
+    //  Date date = resultSet.getDate("datum");
+    //  String comment = resultSet.getString("comments");
+    //  System.out.println("User: " + user);
+    //  System.out.println("Website: " + website);
+    //  System.out.println("summary: " + summary);
+    //  System.out.println("Date: " + date);
+    //  System.out.println("Comment: " + comment);
+    }
+  }
+
+  // You need to close the resultSet
+  private void close() {
+    try {
+      if (resultSet != null) {
+        resultSet.close();
+      }
+
+      if (statement != null) {
+        statement.close();
+      }
+
+      if (connect != null) {
+        connect.close();
+      }
+    } catch (Exception e) {
+
+    }
+  }
+
+} 
